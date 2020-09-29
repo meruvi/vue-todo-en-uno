20115fe01272c31695644b3c28786c74f5d03759
gmeruvia <gmeruvia@cofar.com.bo>
2020-09-29 15:18:00 -0400

    Cambio de nombre de HelloWorld a Carouosel

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Carousel/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Carousel from './components/Carousel.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Carousel
   }
 }
 </script>
